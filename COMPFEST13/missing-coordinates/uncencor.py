f0fc83c7babd5082556a6bb9825214d5e787aacf
Muhammad Alif Putra Yasa <malifputrayasa@gmail.com>
2021-07-29 21:05:42 +0700

    14:05 GMT+7, 29/07/2021

--- COMPFEST13/missing-coordinates/uncencor.py
+++
@@ -1,9 +0,0 @@
-file = open("result.svg")
-wr = open('heheh.svg', 'w')
-kyun = file.readlines()
-a = []
-for i in kyun:
-    if (i.strip()[:5]) == '<path' and len(i.strip()) >= 200:
-        wr.write(i)
-    elif (i.strip()[:5]) != '<path':
-        wr.write(i)
